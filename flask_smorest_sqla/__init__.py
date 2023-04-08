9fb25aca2dc752959123a9f842267e5b116e2b40
Dave <dave@tenko.ai>
2023-04-08 12:31:06 +0200

    use package version

--- flask_smorest_sqla/__init__.py
+++ flask_smorest_sqla/__init__.py
@@ -2,10 +2,7 @@
 
 from .crud_blueprint import CRUDBlueprint  # noqa
 
-import re
-import os
 
-with open(os.path.join(os.path.dirname(__file__), "..", "pyproject.toml")) as f:
-    version = re.search(r'version = "(.*)"', f.read()).group(1)
+import importlib.metadata
 
-__version__ = version
+__version__ = importlib.metadata.version("flask-smorest-sqla")
